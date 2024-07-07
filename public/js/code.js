e1c5585c4243be44de8cb59e13e957dfa816cee8
Maxinz <maxinz@gmail.com>
2024-07-07 12:57:45 -0300

    start development of index.html, js and css. Fetching data from backend

---
+++ public/js/code.js
@@ -0,0 +1,110 @@
+let economyNewsResponse = [];
+
+async function getNewsByCategory(categoria){
+    newsResponse = await fetch(`http://localhost:2408/api/articulos_por_id/${categoria}`);
+    newsArray = await newsResponse.json();
+    return newsArray;
+}
+
+function renderArticles(article, category_id){
+
+    switch (category_id) {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            {
+                const economyArticles_Container = document.querySelector('.economy-articles-container');
+                const economyArticleContainer = document.createElement('article');
+                economyArticleContainer.classList.add('article-card');
+            
+                const imgArticleContainer = document.createElement('div');
+                imgArticleContainer.classList.add('containerImg');
+                const imgElement = document.createElement('img');
+                imgElement.setAttribute("src", article.img);
+                
+                imgArticleContainer.append(imgElement);
+            
+                const titleArticle = document.createElement('h3');
+                titleArticle.textContent = article.titulo;
+                const textArticle = document.createElement('p');
+                textArticle.textContent = article.textoPortada;
+            
+                economyArticleContainer.append(imgArticleContainer,titleArticle,textArticle);
+            
+                economyArticles_Container.append(economyArticleContainer);
+            }
+            break;
+    
+        case 4:
+            {
+                const sportsArticles_Container = document.querySelector('.sports-articles-container');
+                const sportsArticleContainer = document.createElement('article');
+                sportsArticleContainer.classList.add('article-card');
+            
+                const imgArticleContainer = document.createElement('div');
+                imgArticleContainer.classList.add('containerImg');
+                const imgElement = document.createElement('img');
+                imgElement.setAttribute("src", article.img);
+                
+                imgArticleContainer.append(imgElement);
+            
+                const titleArticle = document.createElement('h3');
+                titleArticle.textContent = article.titulo;
+                const textArticle = document.createElement('p');
+                textArticle.textContent = article.textoPortada;
+            
+                sportsArticleContainer.append(imgArticleContainer,titleArticle,textArticle);
+            
+                sportsArticles_Container.append(sportsArticleContainer);
+            }
+            break;
+        case 5:
+            {
+                const techArticles_Container = document.querySelector('.tech-articles-container');
+                const techArticleContainer = document.createElement('article');
+                techArticleContainer.classList.add('article-card');
+            
+                const imgArticleContainer = document.createElement('div');
+                imgArticleContainer.classList.add('containerImg');
+                const imgElement = document.createElement('img');
+                imgElement.setAttribute("src", article.img);
+                
+                imgArticleContainer.append(imgElement);
+            
+                const titleArticle = document.createElement('h3');
+                titleArticle.textContent = article.titulo;
+                const textArticle = document.createElement('p');
+                textArticle.textContent = article.textoPortada;
+            
+                techArticleContainer.append(imgArticleContainer,titleArticle,textArticle);
+            
+                techArticles_Container.append(techArticleContainer);
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+async function populateArticles(category){
+
+    const economyNewsArray = await getNewsByCategory(category);
+    economyNewsArray.forEach(article => {
+        renderArticles(article,category);
+    });
+}
+
+/*
+    los IDs de las categorias son:
+        1 - Actualidad;
+        2 - Policiales;
+        3 - Economía;
+        4 - Deportes;
+        5 - Tecnología
+*/
+
+populateArticles(3);
+populateArticles(4);
+populateArticles(5);
