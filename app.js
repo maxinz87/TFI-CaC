3c8c5d88342245a6e0c398ba2d4a1288c34d81c0
Maxinz <maxinz@gmail.com>
2024-07-12 00:09:30 -0300

    config vercel.json

--- app.js
+++ app.js
@@ -21,9 +21,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(cors({
-    origin: 'https://cac-news.vercel.app'
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.static('public',{index:false}));
 
