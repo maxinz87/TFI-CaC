102d14bf03bd33886de4aad588e7533d2ba70c90
Maxinz <maxinz@gmail.com>
2024-07-12 00:51:12 -0300

    config vercel.json

--- app.js
+++ app.js
@@ -13,20 +13,13 @@ const app = express();
 
 const apiDir = '/api';
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
-
 app.use(cors({
     origin: '*',
     methods: '*',
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   }));
+  
 app.use(express.json());
 app.use(express.static('public',{index:false}));
 
